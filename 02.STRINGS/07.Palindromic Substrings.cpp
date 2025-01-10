dba975d76c81e16659c4c05c7bce7cafcdf2cc12
Purvesh Jambhulkar <130358075+purveshjambhulkar@users.noreply.github.com>
2025-01-10 16:32:40 +0530

    string start

---
+++ 02.STRINGS/07.Palindromic Substrings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int palindromeCount(string s ,int left , int right){
+        int count = 0;
+        while(left >= 0 && right < s.length() && s[left] == s[right]){
+            left--;
+            right++;
+            count++;
+        }
+        return count;
+    }
+
+    int countSubstrings(string s) {
+        int n = s.length();
+        int ans =0;
+        for(int i = 0; i <n ;i++){
+            int even = palindromeCount(s , i , i + 1);
+            int odd = palindromeCount(s , i, i);
+            ans = ans + even + odd;
+        }
+        return ans;
+
+    }
+};
